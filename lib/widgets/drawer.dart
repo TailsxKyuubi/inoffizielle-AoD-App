0b693369e467504297ac9b232db3ab3f32058205
TailsxKyuubi <tailsxkyuubi@web.de>
2020-12-31 23:30:18 +0100

    unnötige dependency entfernt

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -3,7 +3,6 @@
  * This code is part of inoffizielle-AoD-App and licensed under the AGPL License
  */
 import 'package:flutter/material.dart';
-import 'package:unoffical_aod_app/caches/login.dart';
 class DrawerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
