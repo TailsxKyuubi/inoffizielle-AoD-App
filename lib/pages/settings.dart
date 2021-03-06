af91bac9276dabdd884ed4b1c9a53b9c4d74c13c
tailsxkyuubi <tailsxkyuubi@web.de>
2021-03-06 18:55:23 +0100

    Fix für Einstellungsseite

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -16,65 +16,38 @@ import 'package:unoffical_aod_app/widgets/settings/player.dart';
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return
-      RawKeyboardListener(
-          focusNode: FocusNode(),
-          autofocus: true,
-          onKey: (event){
-            print('triggered');
-            FocusScopeNode scope = FocusScope.of(context);
-            if(scope.hasPrimaryFocus){
-              print('has primary focus');
-              scope.requestFocus(appSettingsFocusNodes.first);
-            }else{
-              if( Platform.isAndroid && event.data is RawKeyEventDataAndroid && event.runtimeType == RawKeyUpEvent ){
-                RawKeyEventDataAndroid eventDataAndroid = event.data;
-                switch(eventDataAndroid.keyCode){
-                  case KEY_UP:
-                    appSettingsFocusIndex--;
-                    playerSettingsFocusIndex--;
-                    break;
-                  case KEY_DOWN:
-                    appSettingsFocusIndex--;
-                    playerSettingsFocusIndex--;
-                    break;
-                }
-              }
-            }
-          },
-          child: DefaultTabController(
-              length: 2,
-              child: Scaffold(
-                bottomNavigationBar: NavigationBar(),
-                appBar: AppBar(
-                  title: Text('Einstellungen'),
-                  bottom: TabBar(
-                    unselectedLabelColor: Colors.white,
-                    labelColor: Theme.of(context).accentColor,
-                    tabs: [
-                      Padding(
-                        padding: EdgeInsets.only(bottom: 10,top: 10),
-                        child: Text('App'),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(bottom: 10,top: 10),
-                        child: Text('Player'),
-                      ),
-                    ],
-                  ),
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          bottomNavigationBar: NavigationBar(),
+          appBar: AppBar(
+            title: Text('Einstellungen'),
+            bottom: TabBar(
+              unselectedLabelColor: Colors.white,
+              labelColor: Theme.of(context).accentColor,
+              tabs: [
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10,top: 10),
+                  child: Text('App'),
                 ),
-                body: Container(
-                  height: MediaQuery.of(context).size.height,
-                  child: TabBarView(
-                      children: [
-                        AppSettingsWidget(),
-                        PlayerSettingsWidget()
-                      ]
-                  ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10,top: 10),
+                  child: Text('Player'),
                 ),
-              )
-          )
-      );
+              ],
+            ),
+          ),
+          body: Container(
+            height: MediaQuery.of(context).size.height,
+            child: TabBarView(
+                children: [
+                  AppSettingsWidget(),
+                  PlayerSettingsWidget()
+                ]
+            ),
+          ),
+        )
+    );
   }
 
 }
